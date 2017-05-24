421df29aa7e030ddf666fd5db8e8e98721b58940
vsellathurai <vsellathurai@bloomberg.net>
2017-05-25 04:05:31 +0800

    added more files

--- gensim.py
+++ gensim.py
@@ -5,4 +5,10 @@ Created on Mon May 22 13:38:09 2017
 @author: vsellathurai
 """
 
-from gensim import models
+from gensim.models import doc2vec
+import mainvickfinal as m
+
+fn = m.list_of_all_papers
+model = doc2vec.Doc2Vec.load(fn)
+model.most_similar('pregnant')
+matches = list(filter(lambda x: 'SENT_' in x[0], matches))
